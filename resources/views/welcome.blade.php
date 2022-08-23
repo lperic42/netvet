22995d2030d76cad496729ad896992311a7f326e
Luka <luka.peric@nth.ch>
2022-08-23 13:11:46 +0200

    section about on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,20 @@
             </div>
         </section>
 
+        <section class="section-about">
+            <div class="about">
+                <div class="about--text">
+                    <h2>Zdravlje naših krznenih prijatelja je uvijek  <br> na prvom mjestu</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut fugiat necessitatibus nemo odio qui, quis saepe.
+                        Accusantium adipisci animi aperiam asperiores aut cumque debitis delectus dolores, ea error est eveniet excepturi expedita explicabo
+                        facere harum illo illum itaque iusto laboriosam laborum maiores natus nemo nisi non officiis quae quisquam ratione reiciendis, sed sequi,
+                        sint soluta suscipit tempore temporibus vero voluptatem. Adipisci aperiam, eius excepturi itaque qui quia quibusdam. A, nobis, repellat!
+                        At delectus dignissimos eaque hic neque rem tempore voluptates? Ab alias animi commodi,
+                        consequatur cum cupiditate, in magni molestiae nam officia porro quidem quo rerum similique ut voluptate?</p>
+                </div>
+            </div>
+        </section>
+
 
 
     </div>
