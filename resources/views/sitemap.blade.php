caad4b8449e33ea24604a38691f6fd7ab9c19755
Luka <luka.peric@nth.ch>
2022-10-22 11:21:33 +0200

    updated sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,8 +1,28 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{ url('/') }}/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ url('/o-nama') }}/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ url('/kontakt') }}/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ url('/blog') }}/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
     @foreach ($blogs as $blog)
         <url>
-            <loc>{{ url('/') }}/page/{{ $blog->slug }}</loc>
+            <loc>{{ url('/') }}/{{ $blog->slug }}</loc>
             <lastmod>{{ $blog->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.8</priority>
